0b278269fd732eeffd86f17c04a610e36fe2be2a
Arpit Bansal <arpit.bansal@outlook.com>
2020-08-26 14:29:02 +0530

    VTF API | Code cleanup and positions handling

--- server/routes.js
+++ server/routes.js
@@ -1,34 +1,32 @@
-// import { login, signup } from "./authentication";
-const {login, signup, userList} = require('./authentication');
-const { getStocksList } = require('./stocks');
+const { login, signup, userList } = require("./authentication");
+const { getStocksList, setPosition } = require("./stocks");
 
 const httpEndPoints = {
-    'POST': {
-        '/login': function(req, res, db) {
-            console.log('inside the routes file');
-            login(req, res, db);
-        },
-        '/signUp': function(req, res, db) {
-            console.log('inside the routes file');
-            signup(req, res, db);
-        }
+  POST: {
+    "/login": function (req, res, db) {
+      login(req, res, db);
     },
-    'GET': {
-        '/userList': function(req, res, db) {
-            console.log('inside the routes file for Get call');
-            userList(req, res, db);
-        }
-    }
-}
+    "/signUp": function (req, res, db) {
+      signup(req, res, db);
+    },
+    "/setPosition": function (req, res, db) {
+      setPosition(req, res, db);
+    },
+  },
+  GET: {
+    "/userList": function (req, res, db) {
+      userList(req, res, db);
+    },
+  },
+};
 
 const wsEndPoints = {
-    '/stocks': function(req, res, db) {
-        console.log('inside the routes file for ws stocks call');
-        getStocksList(req, res, db);
-    }
-}
+  "/stocks": function (req, res, db) {
+    getStocksList(req, res, db);
+  },
+};
 
 module.exports = {
-    httpRoutes: httpEndPoints,
-    wsRoutes: wsEndPoints
-}
+  httpRoutes: httpEndPoints,
+  wsRoutes: wsEndPoints,
+};
